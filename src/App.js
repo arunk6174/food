5cc56cee5dbaa8921f1e2d4babeaf75dce6cc26f
Arun Kumar <43911987+arunk6174@users.noreply.github.com>
2022-04-25 21:53:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Meal from './Components/Meal'
+
+
+
+function App() {
+  return(
+    <Meal/>
+  )
+}
+
+export default App;
